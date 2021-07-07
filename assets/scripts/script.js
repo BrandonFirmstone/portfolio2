7d14976f75570cbfecd8c54b098a6b7ec84a0b46
BrandonFirmstone <brandonfirmstone@outlook.com>
2021-07-07 19:55:48 +0000

    INITIAL COMMIT - Created template code, comments and README ready for project start

---
+++ assets/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+
+    INFORMATION REGARDING COMMENTS
+    Each line of comments either annotates the line of code below itself or is used
+    for ease of reading. Comments are used to seperate each part of the program into
+    sections. For example, a contact page may have the contact form seperate from the
+    contact details. They may also be used to essentially draw a line between sections
+    to visually seperate them so that it is easier to read
+
+*/
