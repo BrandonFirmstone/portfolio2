fe18e3fdb91f415d866526aaefc2e6ae0083007c
BrandonFirmstone <brandonfirmstone@outlook.com>
2021-07-31 10:55:15 +0000

    Started game script - changed link on index call to action

---
+++ dungeon-crawler-game/assets/script/dungeon-crawler-script.js
@@ -0,0 +1,41 @@
+const gameTextElement = document.getElementById("dc-text");
+const optionButtonsElement = document.getElementById("dc-option-buttons");
+
+let playerInventory = {};
+let playerClass = "";
+let playerStats = {};
+let playerHealth = 100;
+let playerParty = {};
+
+function startGame() {
+    showGameText(1);
+}
+
+function showGameText() {
+
+}
+
+function selectOption() {
+
+}
+
+const gameText = [
+    {
+        id: 1,
+        text: "Bla bla bla",
+        options: [
+            {
+                text: "bla"
+            },
+            {
+                text: "bla bla"
+            },
+            {
+                text: "bla bla bla"
+            }
+            
+        ]
+    }
+]
+
+startGame();
