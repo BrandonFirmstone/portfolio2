8be4837b7da8d158cf96ad122776aa1f8fe1e702
BrandonFirmstone <brandonfirmstone@outlook.com>
2021-08-11 18:01:32 +0000

    Reorganised common.js to have variables at the top, then functions, then event listeners and then everything else

--- assets/script/common.js
+++ assets/script/common.js
@@ -1,46 +1,62 @@
 
-
 let menuButton = document.getElementById("navigation-toggle");
-menuButton.addEventListener('click', openNavigationMenu);
-
 let closeMenuButton = document.getElementById("overlay-close");
-closeMenuButton.addEventListener('click', closeNavigationMenu);
 
+let modal = document.getElementById("modal-container");
+let modalOpen = document.getElementById("attributions-modal");
+let modalClose = document.getElementById("credits-close");
+
+
+/**
+ * Opens the navigation menu by setting the width to 100%
+ */
 function openNavigationMenu(){
     document.getElementById("overlay").style.width = "100%";
-    console.log("openNavigationMenu ran");
 }
 
+/**
+ * Closes the navigation menu by giving it a width of 0
+ */
 function closeNavigationMenu(){
     document.getElementById("overlay").style.width = "0";
-    console.log("closeNavigationMenu ran");
 }
 
-/* https://stackoverflow.com/questions/3664381/force-page-scroll-position-to-top-at-page-refresh-in-html */
+/**
+ * Sets the credit modal's display to block, revealing it.
+ * https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_modal
+ * Link above helped with making modal. 
+ */
+function showCredits(){
+    modal.style.display = "block";
+}
+
+/**
+ * Hides the credit modal by setting the display to none
+ */
+function hideCredits(){
+    modal.style.display = "none";
+}
+
+
+/**
+ * Checks for the window to be fully loaded before adding the event listeners
+ */
+window.addEventListener('DOMContentLoaded', (event) => {
+    modalOpen.addEventListener('click', showCredits);
+    modalClose.addEventListener('click', hideCredits);
+    closeMenuButton.addEventListener('click', closeNavigationMenu);
+    menuButton.addEventListener('click', openNavigationMenu);
+});
+
+
 /**
  * If scroll history is true it will set it to manual, if not it will scroll to the top of the page on refresh
+ * Source: https://stackoverflow.com/questions/3664381/force-page-scroll-position-to-top-at-page-refresh-in-html
  */
-if (history.scrollRestoration) {
+ if (history.scrollRestoration) {
     history.scrollRestoration = 'manual';
 } else {
     window.onbeforeunload = function () {
         window.scrollTo(0, 0);
     }
 }
-
-let modal = document.getElementById("modal-container");
-let modalOpen = document.getElementById("attributions-modal");
-let modalClose = document.getElementById("credits-close");
-
-function showCredits(){
-    modal.style.display = "block";
-}
-
-function hideCredits(){
-    modal.style.display = "none";
-}
-
-window.addEventListener('DOMContentLoaded', (event) => {
-    modalOpen.addEventListener('click', showCredits);
-    modalClose.addEventListener('click', hideCredits);
-});
